500e611457396f6b4c7c5c71df0bc2ce87a2fd13
“Leonssizm” <levani.kereselidze11@gmail.com>
2022-07-24 21:55:30 +0400

    innitial commit

---
+++ assets/script/scripts.js
@@ -0,0 +1,8 @@
+// landing page "start questionnare btn takes us to next page"
+function startQuestionaryHandler() {
+  window.location.href = "./coordinates.html";
+}
+//submit questionnare
+function submitButtonHandler() {
+  window.location.href = "./thanks.html";
+}
---
+++ assets/script/skillset.js
@@ -0,0 +1,1 @@
+//adding programming language
