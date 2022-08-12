1bb9ac0fd0cc4f3c691f1f174ee3591d81df5eea
“Leonssizm” <levani.kereselidze11@gmail.com>
2022-08-12 18:50:36 +0400

    wip:retrieve and display data from API

--- assets/script/submitted-apps.js
+++ assets/script/submitted-apps.js
@@ -19,5 +19,26 @@ submittedAppsBtn.addEventListener("click", () => {
     "https://bootcamp-2022.devtest.ge/api/applications?token=b3fdeb04-8ed1-4544-93ff-8cf46471498c"
   )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        document.getElementById("personal-info-header").innerHTML =
+          "Personal Information";
+        document.getElementById("first-name").innerHTML = "First Name";
+        document.getElementById("first-name-content").innerHTML =
+          data[i].first_name;
+        document.getElementById("last-name").innerHTML = "Last Name";
+        document.getElementById("last-name-content").innerHTML =
+          data[i].last_name;
+        document.getElementById("email").innerHTML = "E-mail";
+        document.getElementById("email-content").innerHTML = data[i].email;
+        document.getElementById("phone").innerHTML = "Phone";
+        document.getElementById("phone-content").innerHTML = data[i].phone;
+        document.getElementById(
+          "covid-info-header"
+        ).innerHTML = `Covid Situation`;
+        document.getElementById(
+          "work-question-header"
+        ).innerHTML = `how would you prefer to work`;
+      }
+    });
 });
