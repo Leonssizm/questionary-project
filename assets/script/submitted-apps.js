737e988f9c26f653f470b9ba0a65f942cfa93eb1
“Leonssizm” <levani.kereselidze11@gmail.com>
2022-08-18 18:46:20 +0400

    fix(submitted-apps): sent forms dynamically append to the page

--- assets/script/submitted-apps.js
+++ assets/script/submitted-apps.js
@@ -1,98 +1,119 @@
 //making a collapsible list
 
-document.querySelectorAll(".accordion-btn").forEach((button) => {
-  button.addEventListener("click", () => {
-    let accordionContent = button.nextElementSibling;
-    button.classList.toggle("accordion-btn-active");
-    if (button.classList.contains("accordion-btn-active")) {
-      setTimeout(() => {
-        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-      }, 4000);
-    } else {
-      accordionContent.style.maxHeight = 0;
-    }
-  });
-});
-
-//fetching data from the API
-// let submittedAppsBtn = document.getElementById("submittedApps");
-// submittedAppsBtn.onclick = fetchData;
-
+const accordion = document.getElementsByClassName("contentBox");
+setTimeout(function () {
+  for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+    });
+  }
+}, 4000);
 fetch(
   "https://bootcamp-2022.devtest.ge/api/applications?token=" +
     localStorage.getItem("token")
 )
   .then((response) => response.json())
   .then((data) => {
-    for (let i = 0; i < data.length; i++) {
-      //personal info
-      document.getElementById("personal-info-header").innerHTML =
-        "Personal Information";
-      document.getElementById("first-name").innerHTML = "First Name";
-      document.getElementById("first-name-content").innerHTML =
-        data[i].first_name;
-      document.getElementById("last-name").innerHTML = "Last Name";
-      document.getElementById("last-name-content").innerHTML =
-        data[i].last_name;
-      document.getElementById("email").innerHTML = "E-mail";
-      document.getElementById("email-content").innerHTML = data[i].email;
-      document.getElementById("phone").innerHTML = "Phone";
-      document.getElementById("phone-content").innerHTML = data[i].phone;
-
-      //covid situation and work preference
-
-      document.getElementById(
-        "covid-info-header"
-      ).innerHTML = `Covid Situation`;
-      document.getElementById(
-        "work-question-header"
-      ).innerHTML = `how would you prefer to work`;
-      if (data[i].work_preference === "from_home") {
-        document.getElementById("from-office").innerHTML =
-          "&#9711;" + "From Sairme Office";
-        document.getElementById("from-home").innerHTML =
-          "&#10686;" + "From home";
-        document.getElementById("hybrid").innerHTML = "&#9711;" + "Hybrid";
-      } else if (data[i].work_preference === "from_office") {
-        document.getElementById("from-office").innerHTML =
-          "&#10686;" + "From Sairme Office";
-        document.getElementById("from-home").innerHTML =
-          "&#9711;" + "From home";
-        document.getElementById("hybrid").innerHTML = "&#9711;" + "Hybrid";
-      } else if (data[i].work_preference === "hybrid") {
-        document.getElementById("from-office").innerHTML =
-          "&#9711;" + "From Sairme Office";
-        document.getElementById("from-home").innerHTML =
-          "&#9711;" + "From home";
-        document.getElementById("hybrid").innerHTML = "&#10686;" + "Hybrid";
+    let formData = "";
+    let workPreferenceData = "";
+    let covidData = "";
+    let vaccinationData = "";
+    data.map((data) => {
+      //handle work preference dynamic data
+      if (data.work_preference === "from_office") {
+        workPreferenceData = `
+      <div class="work-preference-info">
+        <h4 class="question-header">How Would You Prefer to Work?</h4>
+        <p> &#10686; From Sairme Office</p>
+        <p>&#9711; From Home</p>
+        <p>&#9711; Hybrid</p>
+      </div>`;
+      } else if (data.work_preference === "from_home") {
+        workPreferenceData = `
+        <div class="work-preference-info">
+        <h4 class="question-header">How Would You Prefer to Work?</h4>
+        <p>&#9711; From Sairme Office</p>
+        <p>&#10686; From Home</p>
+        <p>&#9711; Hybrid</p>
+      </div>`;
+      } else if (data.work_preference === "hybrid") {
+        workPreferenceData = `
+        <div class="work-preference-info">
+        <h4 class="question-header">How Would You Prefer to Work?</h4>
+        <p>&#9711; From Sairme Office</p>
+        <p>&#9711; From Home</p>
+        <p>&#10686; Hybrid</p>
+      </div>`;
       }
-
-      // Covid Contact
-
-      document.getElementById("covid-question-header").innerHTML =
-        "Did you have covid 19?";
-
-      if (data[i].had_covid == false) {
-        document.getElementById("covid-yes").innerHTML = "&#9711;" + "Yes";
-        document.getElementById("covid-no").innerHTML = "&#10686;" + "No";
+      //handle covid form dynamic data
+      if (data.had_covid) {
+        covidData = `
+        <h4 class="covid-question-header">Did you have covid 19?</h4>
+        <div class = "covid-question">
+        <p>&#10686; Yes</p>
+        <p>&#9711; No</p>
+        <p class="sub-questions">When did you have covid?</p>
+        <p id="covid-contact-vaccination-date">${data.had_covid_at} <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 7H17M13 1V3M5 1V3M2 3H16C16.5523 3 17 3.44772 17 4V18C17 18.5523 16.5523 19 16 19H2C1.44772 19 1 18.5523 1 18V4C1 3.44772 1.44772 3 2 3ZM13 15H13.002V15.002H13V15ZM9 15H9.002V15.002H9V15ZM5 15H5.002V15.002H5V15ZM13 11H13.002V11.002H13V11ZM9 11H9.002V11.002H9V11ZM5 11H5.002V11.002H5V11Z" stroke="#FE3B1F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        </p>
+        </div>`;
+      } else {
+        covidData = `
+        <h4 class="covid-question-header">Did you have covid 19?</h4>
+        <div class = "covid-question">
+        <p>&#9711; Yes</p>
+        <p>&#10686; No</p>
+        </div>`;
       }
-      if (data[i].had_covid == true) {
-        document.getElementById("covid-yes").innerHTML = "&#10686;" + "Yes";
-        document.getElementById("covid-no").innerHTML = "&#9711;" + "No";
-        document.getElementById("covid-date-title").innerHTML =
-          "When did you have covid?";
-        document.getElementById("covid-contact-date").innerHTML =
-          data[i].had_covid_at + "<hr>" + "&#128197";
+      // handle Vaccination dynamic data
+
+      if (data.vaccinated) {
+        vaccinationData = `
+        <h4 class="vaccine-question-header">Have You been Vaccinated?</h4>
+        <div class="vaccination-question">
+        <p>&#10686; Yes</p>
+        <p>&#9711; No</p>
+        <p class="sub-questions">When did you have covid Vaccine?</p>
+        <p id="covid-contact-vaccination-date">${data.vaccinated_at} <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 7H17M13 1V3M5 1V3M2 3H16C16.5523 3 17 3.44772 17 4V18C17 18.5523 16.5523 19 16 19H2C1.44772 19 1 18.5523 1 18V4C1 3.44772 1.44772 3 2 3ZM13 15H13.002V15.002H13V15ZM9 15H9.002V15.002H9V15ZM5 15H5.002V15.002H5V15ZM13 11H13.002V11.002H13V11ZM9 11H9.002V11.002H9V11ZM5 11H5.002V11.002H5V11Z" stroke="#FE3B1F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        </p>
+        </div>
+        `;
+      } else {
+        vaccinationData = `
+        <h4 class="vaccine-question-header">Have You been Vaccinated?</h4>
+        <div class="vaccination-question">
+        <p>&#9711; Yes</p>
+        <p>&#10686; No</p>
+        </div>
+        `;
       }
+      formData += `
+      <div class="contentBox">
+      <div class="label">${data.first_name}</div>
+      <div class = "content">
+      <h3 class="info-header">Personal Information</h3>
+        <div class="fetched-personal-info">
+          <div class="fetched-info-names">
+          <p> <strong>First Name</strong> ${data.first_name}</p>
+          <p><strong>Last-name</strong> ${data.last_name}</p>
+          <p><strong>email</strong> ${data.email}</p>
+          <p><strong>Phone</strong> ${data.phone}</p>
+          </div>
+        </div>
 
-      // Vaccination
+        <!--Covid Situation-->
+        <h3 class="info-header">Covid situation</h3>
 
-      document.getElementById("vaccine-question-header").innerHTML =
-        "Have you been Vaccinated?";
-
-      if (data[i].vaccinated == true) {
-        document.getElementById("vaccine-yes").innerHTML = "&#10686;" + "Yes";
-        document.getElementById("vaccine-no").innerHTML = "&#9711;" + "No";
-      }
-    }
+        ${workPreferenceData}
+        ${covidData}
+        ${vaccinationData}
+        
+      </div>
+      </div>`;
+    });
+    document.getElementById("accordion").innerHTML = formData;
+    console.log(data);
   });
